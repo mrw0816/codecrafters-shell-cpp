38a3e5ffb0818a14220477cafc2c0692716865e8
Ryan <mrw0816@gmail.com>
2026-02-15 13:33:27 -0700

    basic echo

--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,38 @@
 #include <iostream>
+#include <sstream>
 #include <string>
 
 int main() {
-  // Flush after every std::cout / std:cerr
-  std::cout << std::unitbuf;
-  std::cerr << std::unitbuf;
+    // Flush after every std::cout / std:cerr
+    std::cout << std::unitbuf;
+    std::cerr << std::unitbuf;
 
-  // TODO: Uncomment the code below to pass the first stage
+    // TODO: Uncomment the code below to pass the first stage
 
-  while(true)
-  {
-    std::cout << "$ ";
-    std::string command;
-    std::cin >> command;
-    if(command == "exit")
+    while(true)
     {
-      std::exit(0);
+        std::cout << "$ ";
+        std::string inputText;
+        std::getline(std::cin, inputText);
+
+        std::istringstream input(inputText);
+        std::string command;
+        std::string args;
+
+        input >> command;
+        std::getline(input, args);
+
+        if(command == "exit")
+        {
+            std::exit(0);
+        }
+        else if(command == "echo")
+        {
+            std::cout << args << "\n";
+        }
+        else
+        {
+            std::cout << command << ": command not found" << std::endl;
+        }
     }
-    else
-    {
-      std::cout << command << ": command not found" << std::endl;
-    }
-  }
 }
