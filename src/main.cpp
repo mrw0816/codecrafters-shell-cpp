245226641a20392be288ee3a99acd63f63b59902
Ryan <mrw0816@gmail.com>
2026-02-15 11:57:41 -0700

    implemented command loop

--- src/main.cpp
+++ src/main.cpp
@@ -7,8 +7,12 @@ int main() {
   std::cerr << std::unitbuf;
 
   // TODO: Uncomment the code below to pass the first stage
-  std::cout << "$ ";
-  std::string command;
-  std::cin >> command;
-  std::cout << command << ": command not found" << std::endl;
+
+  while(true)
+  {
+    std::cout << "$ ";
+    std::string command;
+    std::cin >> command;
+    std::cout << command << ": command not found" << std::endl;
+  }
 }
